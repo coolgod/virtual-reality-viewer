523cd608aec87c0dac24e132dd333bb07041ee87
coolgod <realcoolgod@gmail.com>
2016-02-04 15:43:54 -0600

    add littlstar api example

--- test/littlstar.php
+++ test/littlstar.php
@@ -625,10 +625,12 @@ class Littlstar
       'Content-Type: application/json'
     );
     $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $response = curl_exec($ch);
-    return json_decode($response);
+    return json_decode($response, true);
   }
 }
