d5438607d3dc9bf5205d3b00a3cf27f918f22981
coolgod <realcoolgod@gmail.com>
2016-09-08 12:58:41 -0500

    add stats.js

--- js/loading.js
+++ js/loading.js
@@ -65,6 +65,9 @@ function showEntryScene() {
 		});
 		$(document.body).append(canvas);
 	});
+
+	stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+	document.body.appendChild(stats.dom);
 }
 
 initLoadingBar();
